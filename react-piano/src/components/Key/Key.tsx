aa1ef6e7ebbc7fb4c485f54d78c826c0a8a50b14
yuxin-miao <yuxinmiao@sjtu.edu.cn>
2024-10-27 13:52:49 +0200

    map real keys to virtual

--- react-piano/src/components/Key/Key.tsx
+++ react-piano/src/components/Key/Key.tsx
@@ -2,7 +2,7 @@ import styles from "./Key.module.css";
 import { FunctionComponent } from "react";
 import { NoteType } from "../../domain/note";
 import clsx from "clsx";
-
+import { usePressObserver } from "../PressObserver/usePressObserver";
 type PressCallback = () => void;
 type KeyProps = {
 	type: NoteType;
@@ -13,9 +13,18 @@ type KeyProps = {
 };
 export const Key: FunctionComponent<KeyProps> = (props) => {
 	const { type, label, onDown, onUp, ...rest } = props;
+	const pressed = usePressObserver({
+		watchKey: label,
+		onStartPress: onDown,
+		onFinishPress: onUp,
+	});
 	return (
 		<button
-			className={clsx(styles.key, styles[type])}
+			className={clsx(
+				styles.key,
+				styles[type],
+				pressed && styles["is-pressed"]
+			)}
 			onMouseDown={onDown}
 			onMouseUp={onUp}
 			type="button"
