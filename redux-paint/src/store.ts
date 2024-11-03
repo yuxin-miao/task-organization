5dff6a7e46c4482765eec282798a3da44aba7d5c
yuxin-miao <yuxinmiao@sjtu.edu.cn>
2024-11-03 22:39:39 +0200

    setup canvas

---
+++ redux-paint/src/store.ts
@@ -0,0 +1,12 @@
+import { rootReducer } from "./rootReducer";
+import {
+  composeWithDevTools,
+  devToolsEnhancer,
+} from "redux-devtools-extension";
+import { applyMiddleware, createStore } from "redux";
+import { logger } from "redux-logger";
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
